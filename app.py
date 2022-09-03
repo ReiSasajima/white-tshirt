0090610d216b073531165dc2e99858de3f048c9b
AS136-rinrin <rin2022.programing@gmail.com>
2022-09-03 12:12:22 +0000

    flaskひな形

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    return render_template("index.html")
