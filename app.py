82294d22657b6207b5cf76d711157d365df589d0
AS136-rinrin <106909758+AS136-rinrin@users.noreply.github.com>
2022-09-08 07:22:48 +0000

    Thu, Sep 8, 2022, 7:22 AM +00:00

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 import sqlite3
 from cs50 import SQL
+
 # マガポケのデータベースに接続
 conn = sqlite3.connect('magapoke.db')
 db = SQL("sqlite:///magapoke.db")
-conn.close()
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ def index():
         # kyewordと一致する作品をデータベースより見つける
         #book_db = db.execute(
         #    "SELECT title, auther, img FROM magapoke WHERE title LIKE '%?%' OR auther LIKE '%?%'", keyword, keyword)
-        
+
         # 作品が見つからなければNot foundを表示
         title = db.execute("SELECT title FROM magapoke WHERE title LIKE '%?%'", keyword)
         if title == None:
@@ -28,3 +28,4 @@ def index():
         # 作品があれば表示
         return render_template("test.html", title=title)
 
+conn.close()
