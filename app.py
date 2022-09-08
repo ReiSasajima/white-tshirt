a3263e3922adb78bbc893a78a29d20cea7f4dc95
AS136-rinrin <106909758+AS136-rinrin@users.noreply.github.com>
2022-09-08 06:20:34 +0000

    Thu, Sep 8, 2022, 6:20 AM +00:00

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def index():
         # name = "keyword"を取得
         keyword = request.form["keyword"]
         # kyewordと一致する作品をデータベースより見つける
-        title = db.execute("SELECT title FROM magapoke WHERE id = ?", id)
-        auther = db.execute("SELECT auther FROM magapoke WHERE id = ?", id)
-        img = db.execute("SELECT  FROM magapoke WHERE  = ?", id)
+        title = db.execute("SELECT title FROM magapoke WHERE title = keyword")
+        auther = db.execute("SELECT auther FROM magapoke WHERE id = ")
+        img = db.execute("SELECT  img FROM magapoke WHERE  = ")
         return render_template("test.html", keyword=keyword)
 
