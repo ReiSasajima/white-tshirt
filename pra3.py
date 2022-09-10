4de997b1dd605092e732bb634d66a2f787ed95ea
AS136-rinrin <106909758+AS136-rinrin@users.noreply.github.com>
2022-09-10 07:21:57 +0000

    sample

--- pra3.py
+++
@@ -1,5 +0,0 @@
-import chromedriver_binary # <- これでChromeDriverをPATHに自動追加してくれる
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('https://google.com')
--- pra4.py
+++
@@ -1,6 +0,0 @@
-# ege用
-from selenium import webdriver
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
-driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-driver.get('https://google.com')
