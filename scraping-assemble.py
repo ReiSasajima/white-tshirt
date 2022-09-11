ebb682b3ace2cba9d8b7ce459f02cfeea48f81a1
Rei Sasajima <sasajimarei@gmail.com>
2022-09-11 13:44:58 +0900

    add method for cron

--- scraping-assemble.py
+++ scraping-assemble.py
@@ -1,15 +1,23 @@
 #scrapingフォルダから各々のサイト用のスクレイピングモジュールをインポート
 from scraping import booklive, oukoku, cmoa, jumpplus, line, magapoke, piccoma, scraping, ynjn 
-
+from crontab import CronTab
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import os, signal
 import time
 import sqlite3
+
+# インスタンスの生成
+cron = CronTab()
+# ジョブを生成してコマンドを設定する
+job = cron.new(command='python3 ./scraping-assemble.py')
+# 毎週日曜、午前2時にCRONセット
+job.setall('0 2 * * 7')
 # データベースの接続
 conn = sqlite3.connect('manga.db')
 cur = conn.cursor()
+
 
 # option addargumentでブラウザ非表示でselenium実行
 options = Options()
@@ -20,8 +28,9 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=option
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(10)
 
-ynjn() #やんじゃんのスクレイピング関数
-oukoku() #まんが王国のスクレイピング関数
-jumpplus() #ジャンププラスのスクレイピング関数
+def main():
+  ynjn() #やんじゃんのスクレイピング関数
+  oukoku() #まんが王国のスクレイピング関数
+  jumpplus() #ジャンププラスのスクレイピング関数
 
 
