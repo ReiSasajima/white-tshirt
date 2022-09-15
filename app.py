584a3dc75b5ac8c4d9476c6dc09f6a5bc9134137
AS136-rinrin <106909758+AS136-rinrin@users.noreply.github.com>
2022-09-15 07:00:38 +0000

    Thu, Sep 15, 2022, 7:00 AM +00:00

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
 @app.route("/mypage", methods=["GET", "POST"])
 def mypage():
     # sessionを通してログインしているユーザーを確認
-    name = db.execute("SELECT username FROM u)
+    name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
     if request.method == 'GET':
         return render_template("mypage.html")
     elif request.method == 'POST':
