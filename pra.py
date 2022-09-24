7f3006df7784918e947d4750300659b95bcc8228
AS136-rinrin <rin2022.programming@gmail.com>
2022-09-24 07:33:59 +0000

    detailエラー解決

--- pra.py
+++ pra.py
@@ -12,20 +12,12 @@ app = Flask(__name__)
 # 各サービスのテーブル名を含むタプル(プログラム内で変更不可)
 service = ["origin_magapoke", "origin_line", "origin_oukoku"]
 NUM = len(service)
-# sessionの暗号化
-app.secret_key = 'abcdefghijklmn'
-# session継続時間は60分
-app.permanent_session_lifetime = timedelta(minutes=60)
 
 keyword = "犬"
-favorite_book = db.execute("SELECT title FROM favorite WHERE user_id = ? AND like = ?", 7, 1)[0]
-favorite_title = favorite_book
-print(type(favorite_title ))
-print(f"{favorite_title }")
 
-keyword = "犬"
+# 全テーブルから重複しないようにタイトルのみ選択
+titles = db.execute(
+    "SELECT title FROM parent WHERE title LIKE ? OR author LIKE ? GROUP BY title", ('%'+keyword+'%',), ('%'+keyword+'%',))
+# 全テーブルから重複しないようにタイトル、著者、あらすじ、写真を選択
 book_db = db.execute(
-            "SELECT title, author, img_url, summary FROM origin_magapoke WHERE title LIKE ? OR author LIKE ?", ('%'+keyword+'%',), ('%'+keyword+'%',))
-print(type(book_db))
-print(book_db)
-
+    "SELECT title, author, img_url, summary FROM origin_magapoke WHERE title ?", titles)
