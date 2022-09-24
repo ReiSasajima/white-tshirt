4e6f6f47739228a41a75bdbf4c4f1ce6fd600872
AS136-rinrin <rin2022.programming@gmail.com>
2022-09-24 07:53:25 +0000

    Sat, Sep 24, 2022, 7:53 AM +00:00

--- pra.py
+++ pra.py
@@ -1,13 +1,8 @@
-from flask import Flask, redirect, render_template, request, session
-from datetime import timedelta
-from werkzeug.security import check_password_hash, generate_password_hash
 # cs50のライブラリでSQLを操作している。
 from cs50 import SQL
 
 # マガポケのデータベースに接続
 db = SQL("sqlite:///manga.db")
-
-app = Flask(__name__)
 
 # 各サービスのテーブル名を含むタプル(プログラム内で変更不可)
 service = ["origin_magapoke", "origin_line", "origin_oukoku"]
