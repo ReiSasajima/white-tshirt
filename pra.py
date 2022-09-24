4253aa919eb5ce7460f6b7368626f5c58b6c5700
AS136-rinrin <rin2022.programming@gmail.com>
2022-09-24 08:54:44 +0000

    Sat, Sep 24, 2022, 8:54 AM +00:00

--- pra.py
+++ pra.py
@@ -12,7 +12,9 @@ keyword = "犬"
 # ペアレントテーブルから重複しないようにタイトルのみ選択
 titles = db.execute(
     "SELECT title FROM parent WHERE title LIKE ? OR author LIKE ? GROUP BY title", ('%'+keyword+'%',), ('%'+keyword+'%',))
-print(titles[0]["title"])
+for i in len(titles):
+    print(i)
+
 # ペアレントテーブルから重複しないようにタイトル、著者、あらすじ、写真を選択
 #book_db = db.execute(
 #    "SELECT title, author, img_url, summary FROM origin_magapoke WHERE title ?", titles)
